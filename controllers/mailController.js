d4904c34161c78c924eae37f17a2f51b07451da7
harishh2h <h2hharish6@gmail.com>
2021-11-23 14:55:32 +0530

    Mail Controlller & Page Designed

---
+++ controllers/mailController.js
@@ -0,0 +1,51 @@
+const nodemailer            = require('nodemailer');
+const {google}              = require('googleapis');
+const OAuth2                = google.auth.OAuth2;
+
+const createTransporter = async () => {
+    const oauth2Client = new OAuth2(
+      process.env.CLIENT_ID,
+      process.env.CLIENT_SECRET,
+      "https://developers.google.com/oauthplayground"
+    );
+  
+    oauth2Client.setCredentials({
+      refresh_token: process.env.REFRESH_TOKEN
+    });
+  
+    const accessToken = await new Promise((resolve, reject) => {
+      oauth2Client.getAccessToken((err, token) => {
+        if (err) {
+          reject("Failed to create access token :(");
+        }
+        resolve(token);
+      });
+    });
+  
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.EMAIL,
+        accessToken,
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        refreshToken: process.env.REFRESH_TOKEN
+      }
+    });
+  
+    return transporter;
+  };
+  
+  const sendEmail = async (email) => {
+    let emailTransporter = await createTransporter();
+    await emailTransporter.sendMail({
+        subject: "Test 2",
+        text: "I am sending an email from nodemailer!",
+        to: email,
+        from: process.env.EMAIL
+      }).then(result=>{console.log(result)}).catch(err=>{console.log(err)})
+    
+  };
+  
+module.exports = sendEmail
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
-var express = require('express');
-var router = express.Router();
+var express                     = require('express');
+var router                      = express.Router();
 const {ensureAuth, ensureGuest} = require('../middleware/gAuth');
-const nodemailer = require('nodemailer');
+const sendEmail                      = require('../controllers/mailController');
 
 const { NotFound } = require('http-errors');
 const {body,validationResult} = require('express-validator');
 
+
+
+router.get('/l',(req,res,next)=>{
+  res.render('launch');
+})
 
 /* GET home page. */
 router.get('/',ensureGuest, function(req, res, next) {
@@ -13,30 +18,10 @@ router.get('/',ensureGuest, function(req, res, next) {
 });
 
 router.get('/welcome',ensureAuth,async (req,res)=>{
-    console.log('user email',req.user.email);
-
-    let transporter = nodemailer.createTransport({
-      service:'gmail',
-      auth: {
-        user:'adminMAIL',
-        pass:'adminMailPass'
-      }
-    }); 
-
-    var mailOptions = {
-      from: 'adminMail',
-      to: req.user.email,
-      subject:'testing node mail',
-      text:'testing node mail for ahb jobs'
-    };
-
-    transporter.sendMail(mailOptions,(error,info)=>{
-      if(error){
-        console.log(error);
-      }else{
-        console.log('Email sent',+info.response);
-      }
-    });
+    // mail('testUser',req.user.email);
+    // console.log('user email',req.user.email);
+    sendEmail(req.user.email)
+    
     res.json({user:req.user})
  })
 
